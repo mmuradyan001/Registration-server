45f4bd008874ab097bdaea91805440f530391be5
mmuradyan001 <139633503+mmuradyan001@users.noreply.github.com>
2024-01-26 04:08:39 +0400

    Update LoginController.js

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,35 +1,3 @@
-// const jwt = require('jsonwebtoken')
-
-// const createToken = email => {
-//   try {
-//     const token = jwt.sign({email}, process.env.SECRET_WORD, {
-//       expiresIn:'1d'
-//     })
-//     return token
-//   } catch (error) {
-//     console.log("Token creating error", error)
-//     throw error
-//   }
-// }
-
-// class LoginController {
-
-//   async loginUser (req, res) {
-//     const { email, password } = req.body
-//     try {
-//       const user = await req.app.services.login.loginUser(email, password)
-//       const token = createToken(user.email)
-//       res.json({token})
-//       res.json({user})
-//     } catch (error) {
-//       res.status(500).json({ message: error.message})
-//     }
-//   }
-// }
-
-// module.exports = LoginController
-
-
 const jwt = require('jsonwebtoken');
 
 const createToken = (email) => {
